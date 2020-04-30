4952efcc2bedd43c689ec7d7aa26beb8a537f6e9
Niclas Snellman <nici@bambuser.com>
2020-04-30 08:30:31 +0200

    Implement a function that returns a working URL for a random image

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const agent = require('superagent');
+
+const getDogImageURL = async () => {
+  const { body: { message: dogUrl }} = await agent.get('https://dog.ceo/api/breeds/image/random');
+  return dogUrl;
+};
+
 (async () => {
   console.log('hello!');
 })();
