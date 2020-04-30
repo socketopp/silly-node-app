8dd8256c14ba2ca14e63d64f33d9a91d4c6b8edb
Niclas Snellman <nici@bambuser.com>
2020-04-30 08:41:34 +0200

    Print an ascii-fied image of a dog to console

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const agent = require('superagent');
+const asciify = require('asciify-image');
 
 const getDogImageURL = async () => {
   const { body: { message: dogUrl }} = await agent.get('https://dog.ceo/api/breeds/image/random');
@@ -6,5 +7,19 @@ const getDogImageURL = async () => {
 };
 
 (async () => {
-  console.log('hello!');
+  // Get a URL for a random image of a dog
+  const dogUrl = await getDogImageURL();
+
+  // Download the image
+  const { body: data } = await agent.get(dogUrl);
+
+  // Convert the image to ascii
+  const ascii = await asciify(data, {
+    fit: 'box',
+    width: 60,
+    height: 60,
+  });
+
+  // Print the ascii
+  console.log(ascii);
 })();
